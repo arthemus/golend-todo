b2111b8ccd4bf200bac1baf1305bed8ab6c46d1d
Arthemus <arthemus.moreira@gmail.com>
2019-12-22 21:17:40 +1000

    Defining todo model.

---
+++ src/models/todo.model.js
@@ -0,0 +1,10 @@
+class Todo {
+  constructor ({ id, created, message, due }) {
+    this.id = id
+    this.created = created
+    this.message = message
+    this.due = due
+  }
+}
+
+module.exports = Todo
