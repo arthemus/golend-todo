fcbbae1dee5066ccb6d976c4e46cf4f5b7162a4e
Arthemus <arthemus.moreira@gmail.com>
2019-12-23 23:21:44 +1000

    Using json file as database.

--- src/repositories/todo.repository.js
+++ src/repositories/todo.repository.js
@@ -13,15 +13,28 @@ class TodoRepository {
   }
 
   static init () {
-    const file = path.resolve(__dirname, './../../data/mock-todos.json')
-    const todoMockdata = JSON.parse(fs.readFileSync(file, 'utf8'))
+    let todoMockdata
+    let file = path.resolve(__dirname, './../../.db')
+    if (fs.existsSync(file)) {
+      todoMockdata = JSON.parse(fs.readFileSync(file, 'utf8'))
+    } else {
+      file = path.resolve(__dirname, './../../data/mock-todos.json')
+      todoMockdata = JSON.parse(fs.readFileSync(file, 'utf8'))
+    }
     return new TodoRepository(todoMockdata)
+  }
+
+  sync () {
+    const stringData = JSON.stringify(this.data)
+    const file = path.resolve(__dirname, './../../.db')
+    fs.writeFileSync(file, stringData, 'utf8')
   }
 
   create (data) {
     const item = this.data.find(o => o.id === data.id)
     if (item) return
     this.data.push(data)
+    this.sync()
   }
 
   read (id) {
@@ -36,12 +49,14 @@ class TodoRepository {
   update (data) {
     const index = this.data.findIndex(o => o.id === data.id)
     this.data[index] = data
+    this.sync()
   }
 
   delete (id) {
     const index = this.data.findIndex(o => o.id === id)
     if (index > -1) {
       this.data.splice(index, 1)
+      this.sync()
     }
   }
 }
